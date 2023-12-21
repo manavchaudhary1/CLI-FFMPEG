d7644811d54f6b4ee0df5ada8f8b3017ba292337
manav_chaudhary <manavchaudhary041@gmail.com>
2023-12-21 15:03:57 +0530

    Minor patch

--- src/main/java/com/ffmpeg/cli/FFMPEGCommands.java
+++ src/main/java/com/ffmpeg/cli/FFMPEGCommands.java
@@ -19,8 +19,8 @@ public class FFMPEGCommands{
         executeCommand(ffmpegCommand);
     }
 
-    public static void burnSubtitleIntoVideo(String inputFilePath, String outputFilePath, String inputFilePathSub) throws IOException {
-        String ffmpegCommand = "ffmpeg -i " + inputFilePath + " -vf subtitle=" + inputFilePathSub + " " + outputFilePath;
+    public static void burnSubtitleIntoVideo(String inputFilePath, String inputFilePathSub, String outputFilePath) throws IOException {
+        String ffmpegCommand = "ffmpeg -i " + inputFilePath + " -vf subtitles=" + inputFilePathSub + " " + outputFilePath;
         executeCommand(ffmpegCommand);
     }
 
@@ -33,28 +33,31 @@ public class FFMPEGCommands{
         ProcessBuilder processBuilder = new ProcessBuilder(command.split("\\s+"));
         processBuilder.redirectErrorStream(true);  // Redirect error stream to input stream
 
-        Process process = processBuilder.start();
+        try {
+            Process process = processBuilder.start();
 
-        // Read the combined output and error streams
-        try (InputStream inputStream = process.getInputStream();
-             InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-             BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
+            // Read the combined output and error streams
+            try (InputStream inputStream = process.getInputStream();
+                 InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                 BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
 
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                System.out.println(line);
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    System.out.println(line);
+                }
+
+                int exitCode = process.waitFor();
+                if (exitCode != 0) {
+                    throw new IOException("Command execution failed with exit code " + exitCode);
+                }
+
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
             }
-
-            int exitCode = process.waitFor();
-            if (exitCode != 0) {
-                throw new IOException("Command execution failed with exit code " + exitCode);
-            }
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
+        } catch (IOException e) {
+            // Log the entire exception, including the stack trace
+            e.printStackTrace();
+            throw e;  // rethrow the exception for higher-level handling
         }
     }
-
-
-
-
 }
--- src/main/java/com/ffmpeg/cli/Main.java
+++ src/main/java/com/ffmpeg/cli/Main.java
@@ -20,35 +20,53 @@ public class Main {
         try {
             switch (choice) {
                 case 1:
+                    //Convert Video to audio
                     System.out.println("Enter location of video with ext:");
                     String inputFilePathAudio = sc.next();
+
                     System.out.println("Enter location where you want output with file name & '.aac' ext:");
                     String outputFilePathAudio = sc.next();
+
                     FFMPEGCommands.convertVideoToAudio(inputFilePathAudio, outputFilePathAudio);
                     break;
                 case 2:
+                    //Encode Soft subs to video
                     System.out.println("Enter location of video with ext:");
                     String inputFilePathEncode = sc.next();
+
                     System.out.println("Enter location of soft subtitle with ext:");
                     String inputFilePathSubEncode = sc.next();
+
                     System.out.println("Enter location where you want output with file name & ext:");
                     String outputFilePathEncode = sc.next();
+
                     FFMPEGCommands.encodeSoftSubtitleIntoVideo(inputFilePathEncode, outputFilePathEncode, inputFilePathSubEncode);
                     break;
                 case 3:
+                    //Encode Hard subs to video
                     System.out.println("Enter location of video with ext:");
                     String inputFilePathBurn = sc.next();
+
+                    System.out.println("------------NOTE:Please enter drive in this format D\\:/path/to/your/file.srt-----------");
+                    System.out.println("Otherwise prasing error");
+
                     System.out.println("Enter location of soft subtitle with ext:");
                     String inputFilePathSubBurn = sc.next();
+
                     System.out.println("Enter location where you want output with file name & ext:");
                     String outputFilePathBurn = sc.next();
-                    FFMPEGCommands.burnSubtitleIntoVideo(inputFilePathBurn, outputFilePathBurn, inputFilePathSubBurn);
+
+                    FFMPEGCommands.burnSubtitleIntoVideo(inputFilePathBurn, inputFilePathSubBurn, outputFilePathBurn);
                     break;
+
                 case 4:
+                    //Extract soft subs from video
                     System.out.println("Enter location of video with ext:");
                     String inputFilePathExtract = sc.next();
+
                     System.out.println("Enter location where you want output with file name & '.srt' ext:");
                     String outputFilePathExtract = sc.next();
+
                     FFMPEGCommands.extractSubtitleFromVideo(inputFilePathExtract, outputFilePathExtract);
                     break;
                 case 5:
