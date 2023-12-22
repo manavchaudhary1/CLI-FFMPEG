fee1b1dd5b69b78a1355c5b8ff9ec1616f9f40cc
manav_chaudhary <manavchaudhary041@gmail.com>
2023-12-22 22:34:01 +0530

    Added MATROSKA(MKV) Format Support

--- src/main/java/com/ffmpeg/cli/FFMPEGCommands/AddSoftSubtitle.java
+++ src/main/java/com/ffmpeg/cli/FFMPEGCommands/AddSoftSubtitle.java
@@ -1,12 +1,13 @@
 package com.ffmpeg.cli.FFMPEGCommands;
 
 import com.ffmpeg.cli.ProcessBuild;
-
 import java.io.IOException;
 
 public class AddSoftSubtitle {
-    public static void encodeSoftSubtitleIntoVideo(String inputFilePath, String outputFilePath,String inputFilePathSub) throws IOException {
-        String ffmpegCommand ="ffmpeg -i " + inputFilePath + " -i " + inputFilePathSub + " -c:s mov_text -c:v libx264 -c:a copy " + outputFilePath;
+    public static void encodeSoftSubtitleIntoVideo(String inputFilePath, String outputFilePath, String inputFilePathSub) throws IOException {
+        String videoCodec = inputFilePathSub.matches("(?i).*mp4") ? "libx264" : "libx265";
+        String ffmpegCommand = "ffmpeg -i " + inputFilePath + " -i " + inputFilePathSub + " -c:s srt -c:v " + videoCodec + " -c:a copy " + outputFilePath;
         ProcessBuild.executeCommand(ffmpegCommand);
     }
 }
+
