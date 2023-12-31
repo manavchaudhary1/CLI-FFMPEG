53bf5d3945668d946e9f2406dfb8c0c45c2a55ff
manav_chaudhary <100396248+manavchaudhary1@users.noreply.github.com>
2024-01-01 04:33:02 +0530

    Update Main.java

--- src/main/java/com/ffmpeg/cli/Main.java
+++ src/main/java/com/ffmpeg/cli/Main.java
@@ -2,8 +2,6 @@ package com.ffmpeg.cli;
 
 public class Main {
     public static void main(String[] args) {
-        //Switch switchInstance = new Switch();
-        //switchInstance.choice();
         Switch.choice();
     }
 }
